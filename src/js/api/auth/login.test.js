5f2250b0d486d1cc7168fcd38d16d341dce6a1ec
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-11-09 15:16:52 +0100

    fixed the format, lint and lint-fix in the package.json file

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,71 @@
+// LOGIN TEST JS
+
+import { login } from './login';
+
+const TestingTheMail = 'kardo@noroff.no';
+
+const TestingThePassword = 'kardokiyani1998';
+
+const TestingSuccess = {
+  name: 'Kardo',
+  email: 'kardo@noroff.no',
+};
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+function fetchSuccess() {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: 'OK',
+    json: () => Promise.resolve(TestingSuccess),
+  });
+}
+
+function fetchInvalidLogin() {
+  return Promise.resolve({
+    ok: false,
+    status: 401,
+    statusText: 'Unauthorized',
+  });
+}
+
+describe('login', () => {
+  it('Returns a valid access token in local storage and valid response object', async () => {
+    global.fetch = jest.fn(() => fetchSuccess());
+    const expectedToken = TestingSuccess.accessToken;
+    const response = await login(TestingTheMail, TestingThePassword);
+    const storedToken = JSON.parse(localStorage.getItem('token'));
+    expect(storedToken).toEqual(expectedToken);
+    expect(response).toEqual(TestingSuccess);
+  });
+
+  it('Throws error message on invalid details', async () => {
+    global.fetch = jest.fn(() => fetchInvalidLogin());
+    await expect(login(TestingTheMail, TestingThePassword)).rejects.toThrow(
+      'Unauthorized'
+    );
+  });
+});
