8a9f7d5818b2f061dbd79af550a78df69c2a8b56
Kardo Kiyani <91535515+kardokiyani@users.noreply.github.com>
2022-11-13 23:39:14 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
