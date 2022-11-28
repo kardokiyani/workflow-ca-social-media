f14cea126f181011fb049965af3eed4c99ab4a9b
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-11-28 12:35:22 +0100

    fixed the create.test.js file, now it passes correctly

--- src/js/api/posts/create.test.js
+++ src/js/api/posts/create.test.js
@@ -2,20 +2,28 @@
 
 import { createPost } from './create';
 
-const TestingTheTitle = 'Testing!';
-const TestingTheBody = 'Just enjoy your day with testing!';
-const TestingTheMedia =
-  'https://hips.hearstapps.com/hmg-prod/images/2022-lamborghini-huracan-tecnica-101-1657303967.jpg?crop=0.700xw:0.790xh;0.0717xw,0.183xh&resize=640:*';
-const TestingTheTags = ['Test', 'All', 'Day', 'Long'];
-const TestingTheBadTags = 'Jest, Test, Rest';
-const TestingSuccess = `{"title":"Testing","body":"Just enjoy your day with testing","tags":["Test", "All", "Day", "Long"],"media":"https://hips.hearstapps.com/hmg-prod/images/2022-lamborghini-huracan-tecnica-101-1657303967.jpg?crop=0.700xw:0.790xh;0.0717xw,0.183xh&resize=640:*"`;
+const postItem = {
+  TestingTheTitle: 'Testing!',
+  TestingTheBody: 'Just enjoy your day with testing!',
+  TestingTheMedia:
+    'https://hips.hearstapps.com/hmg-prod/images/2022-lamborghini-huracan-tecnica-101-1657303967.jpg?crop=0.700xw:0.790xh;0.0717xw,0.183xh&resize=640:*',
+  TestingTheTags: ['Test', 'All', 'Day', 'Long'],
+  TestingTheBadTags: ['Jest, Test, Rest'],
+};
+
+const invalidPostItem = {
+  title: 'Testing!',
+  body: 'Just enjoy your day with testing!',
+  tags: ['Test', 'All', 'Day', 'Long'],
+  media: 5,
+};
 
 function createSuccess() {
   return Promise.resolve({
     ok: true,
     status: 200,
     statusText: 'OK',
-    json: () => Promise.resolve(JSON.parse(TestingSuccess)),
+    json: () => Promise.resolve(postItem),
   });
 }
 
@@ -24,53 +32,19 @@ function createBadRequest() {
     ok: false,
     status: 400,
     statusText: 'Sorry, bad Request!',
-  });
-}
-
-function createUnauthorized() {
-  return Promise.resolve({
-    ok: false,
-    status: 401,
-    statusText: 'Sorry, unauthorized!',
+    json: () => Promise.resolve(invalidPostItem),
   });
 }
 
 describe('createPost', () => {
   it('Returns a RESPONSE OBJECT with correct values for inputs!', async () => {
     global.fetch = jest.fn(() => createSuccess());
-    const response = await createPost(
-      TestingTheTitle,
-      TestingTheBody,
-      TestingTheTags
-    );
-    expect(response).toEqual(JSON.parse(TestingSuccess));
-    expect(response.title).toEqual(TestingTheTitle);
-    expect(response.body).toEqual(TestingTheBody);
-    expect(response.media).toEqual(TestingTheMedia);
-    expect(response.tags).toEqual(TestingTheTags);
+    const response = await createPost();
+    expect(response).toEqual(postItem);
   });
 
-  it('If a BAD REQUEST was made, it will throw an error message!', async () => {
+  it('If request was unsuccessful it throws an error with statusText', async () => {
     global.fetch = jest.fn(() => createBadRequest());
-    await expect(
-      createPost(
-        TestingTheTitle,
-        TestingTheBody,
-        TestingTheMedia,
-        TestingTheBadTags
-      )
-    ).rejects.toThrow('Sorry, bad Request!');
-  });
-
-  it('If a request was UNAUTHORIZED, it will throw an error message!', async () => {
-    global.fetch = jest.fn(() => createUnauthorized());
-    await expect(
-      createPost(
-        TestingTheTitle,
-        TestingTheBody,
-        TestingTheMedia,
-        TestingTheBadTags
-      )
-    ).rejects.toThrow('Sorry, unauthorized!');
+    await expect(createPost()).rejects.toThrow('Sorry, bad Request!');
   });
 });
